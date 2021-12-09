4ded9095e2f6686ce5e83e900e69c0658e7bae43
Ranjit Nimblakar <ranjitraje.cse@gmail.com>
2021-12-09 12:55:29 +0530

    cmt

---
+++ spring-mysql-db-app/src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,55 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	private int id;
+	private String name;
+	private double age;
+	private double salary;
+	
+	public Employee(int id, String name, double age, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getAge() {
+		return age;
+	}
+
+	public void setAge(double age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+}
